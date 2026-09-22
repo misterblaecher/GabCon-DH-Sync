8b3af675819a0a7e2f4e29ecb67b397b00361963
misterblaecher <63530663+misterblaecher@users.noreply.github.com>
2026-09-22 16:38:39 +0200

    fix: quote SQLite identifiers correctly

--- src/main/java/be/gabcon/dhsync/server/DhDeltaBuilder.java
+++ src/main/java/be/gabcon/dhsync/server/DhDeltaBuilder.java
@@ -310,7 +310,7 @@ public final class DhDeltaBuilder {
     }
 
     private static String qident(String value) {
-        return """ + value.replace(""", """") + """;
+        return "\"" + value.replace("\"", "\"\"") + "\"";
     }
 
     private static String sqliteQuote(String value) {
