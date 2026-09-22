eb5553bc0788fa94bd4a8c5381789f6ee281f84c
misterblaecher <63530663+misterblaecher@users.noreply.github.com>
2026-09-22 15:52:20 +0200

    feat: add serialized snapshot worker

--- src/main/java/be/gabcon/dhsync/server/ServerState.java
+++ src/main/java/be/gabcon/dhsync/server/ServerState.java
@@ -1,6 +1,17 @@
 package be.gabcon.dhsync.server;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public final class ServerState {
     public static final ChangedRegionTracker CHANGED_REGIONS = new ChangedRegionTracker();
+    public static final AtomicBoolean SNAPSHOT_RUNNING = new AtomicBoolean(false);
+    public static final ExecutorService SNAPSHOT_EXECUTOR = Executors.newSingleThreadExecutor(r -> {
+        Thread thread = new Thread(r, "GabConDHSync-Snapshot");
+        thread.setDaemon(true);
+        return thread;
+    });
+
     private ServerState() {}
 }
