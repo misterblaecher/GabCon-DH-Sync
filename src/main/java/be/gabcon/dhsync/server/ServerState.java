2efb06143d47533455dcd6eaf7433d98ada37449
misterblaecher <63530663+misterblaecher@users.noreply.github.com>
2026-09-22 14:12:36 +0200

    feat: add server state

---
+++ src/main/java/be/gabcon/dhsync/server/ServerState.java
@@ -0,0 +1,6 @@
+package be.gabcon.dhsync.server;
+
+public final class ServerState {
+    public static final ChangedRegionTracker CHANGED_REGIONS = new ChangedRegionTracker();
+    private ServerState() {}
+}
