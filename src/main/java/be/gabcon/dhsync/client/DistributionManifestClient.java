a05c61bb04c029c9987cb8aaec4d605c6d72417d
misterblaecher <63530663+misterblaecher@users.noreply.github.com>
2026-09-25 17:49:47 +0200

    fix: propagate checked manifest fetch errors

--- src/main/java/be/gabcon/dhsync/client/DistributionManifestClient.java
+++ src/main/java/be/gabcon/dhsync/client/DistributionManifestClient.java
@@ -2,6 +2,7 @@ package be.gabcon.dhsync.client;
 
 import be.gabcon.dhsync.distribution.DistributionManifest;
 import be.gabcon.dhsync.distribution.DistributionManifestCodec;
+import be.gabcon.dhsync.manifest.ManifestException;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -15,7 +16,7 @@ import java.time.Duration;
 public final class DistributionManifestClient {
     public static final long MAX_MANIFEST_BYTES = 4L * 1024 * 1024;
 
-    public static DistributionManifest fetch(URI uri, long maxAssetBytes) throws IOException, InterruptedException {
+    public static DistributionManifest fetch(URI uri, long maxAssetBytes) throws IOException, InterruptedException, ManifestException {
         if (!"https".equalsIgnoreCase(uri.getScheme()) || uri.getHost() == null) {
             throw new IOException("Manifest URL must use HTTPS");
         }
